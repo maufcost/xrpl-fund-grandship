8ff60727be5ad392940c134a917ccb8888ff1627
Mauricio <mauriciofigueiredo@knights.ucf.edu>
2021-10-07 21:32:39 -0400

    Added classic car marketplace files

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -4,7 +4,27 @@ import { navigate } from '@reach/router'
 
 import './home.css'
 
+// General static assets
 import ArrowRight from '../../assets/arrow-right.svg'
+import Pin from '../../assets/pin.svg'
+
+// Sample classic car listings
+import CarDodgeCharger1968 from '../../assets/classic-cars-sample/1968-dodge-charger.jpeg'
+import Corvette571 from '../../assets/classic-cars-sample/cv57-1.jpeg'
+import Corvette572 from '../../assets/classic-cars-sample/cv57-2.jpeg'
+import Corvette573 from '../../assets/classic-cars-sample/cv57-3.jpeg'
+import Corvette574 from '../../assets/classic-cars-sample/cv57-4.jpeg'
+import Corvette575 from '../../assets/classic-cars-sample/cv57-5.jpeg'
+import Corvette576 from '../../assets/classic-cars-sample/cv57-6.jpeg'
+import Corvette64 from '../../assets/classic-cars-sample/cv64.jpeg'
+import MustangShelby from '../../assets/classic-cars-sample/mustang-shelby-gt-350-65.jpeg'
+import Maverick75 from '../../assets/classic-cars-sample/mv.jpeg'
+import Maverick275 from '../../assets/classic-cars-sample/mv2.jpeg'
+import ShelbyCobra from '../../assets/classic-cars-sample/shelby-cobra-1965.jpeg'
+
+// Sample real state listings
+
+// https://youtu.be/1JayNoKqdl4
 
 class Home extends React.Component {
     constructor(props) {
@@ -14,10 +34,146 @@ class Home extends React.Component {
             // listings: [].
             // @warning: development only:
             carListings: [
-                {}
+                {
+                    img: CarDodgeCharger1968,
+                    name: "Dodge Charger 1968",
+                    location: "USA",
+                    fullPrice: "84,000",
+                    sharePrice: "21,000",
+                    shareType: "1/4 of total cost",
+                    xrp: "7,818.22"
+                },
+                {
+                    img: Corvette571,
+                    additionalImgs: [
+                        Corvette571, Corvette572,
+                        Corvette573, Corvette574,
+                        Corvette575, Corvette576
+                    ],
+                    name: "Chevrolet Corvette 1957",
+                    location: "Berlin",
+                    fullPrice: "119,000",
+                    sharePrice: "19,830",
+                    shareType: "1/6 of total cost",
+                    xrp: "110,758.11"
+                },
+                {
+                    img: Maverick275,
+                    name: "Ford Maverick 1975",
+                    location: "Brazil",
+                    fullPrice: "36,500",
+                    sharePrice: "9,125",
+                    shareType: "1/4 of total cost",
+                    xrp: "33,972.02"
+                },
+                {
+                    img: MustangShelby,
+                    name: "Mustang Shelby",
+                    location: "USA",
+                    fullPrice: "73,260",
+                    sharePrice: "9,157.5",
+                    shareType: "1/8 of total cost",
+                    xrp: "68,186.04"
+                },
+                {
+                    img: Maverick75,
+                    name: "For Maverick 1975",
+                    location: "Chile",
+                    fullPrice: "42,910",
+                    sharePrice: "7,151.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "39,938.07"
+                },
+                {
+                    img: ShelbyCobra,
+                    name: "Shelby Cobra 1960s",
+                    location: "Mexico",
+                    fullPrice: "1,499,000",
+                    sharePrice: "74,950",
+                    shareType: "1/20 of total cost",
+                    xrp: "1,395,179.84"
+                },
+                {
+                    img: Corvette64,
+                    name: "Chevrolet Corvette 1964",
+                    location: "USA",
+                    fullPrice: "82,360",
+                    sharePrice: "13,726.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "76,655.78"
+                },
             ],
             realStateListings: [
-
+                {
+                    img: "",
+                    name: "",
+                    location: "",
+                    specs: "",
+                    fullPrice: "82,360",
+                    sharePrice: "13,726.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "76,655.78"
+                },
+                {
+                    img: "",
+                    name: "",
+                    location: "",
+                    specs: "",
+                    fullPrice: "82,360",
+                    sharePrice: "13,726.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "76,655.78"
+                },
+                {
+                    img: "",
+                    name: "",
+                    location: "",
+                    specs: "",
+                    fullPrice: "82,360",
+                    sharePrice: "13,726.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "76,655.78"
+                },
+                {
+                    img: "",
+                    name: "",
+                    location: "",
+                    specs: "",
+                    fullPrice: "82,360",
+                    sharePrice: "13,726.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "76,655.78"
+                },
+                {
+                    img: "",
+                    name: "",
+                    location: "",
+                    specs: "",
+                    fullPrice: "82,360",
+                    sharePrice: "13,726.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "76,655.78"
+                },
+                {
+                    img: "",
+                    name: "",
+                    location: "",
+                    specs: "",
+                    fullPrice: "82,360",
+                    sharePrice: "13,726.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "76,655.78"
+                },
+                {
+                    img: "",
+                    name: "",
+                    location: "",
+                    specs: "",
+                    fullPrice: "82,360",
+                    sharePrice: "13,726.67",
+                    shareType: "1/6 of total cost",
+                    xrp: "76,655.78"
+                },
             ],
             hotWalletAddress: ''
         }
@@ -34,9 +190,9 @@ class Home extends React.Component {
         return (
             <div className="home-container">
                 {showCarsHome ? (
-                    <ClassicCarsHome />
+                    <ClassicCarsHome listings={this.state.carListings} />
                 ) : (
-                    <RealStateHome />
+                    <RealStateHome listings={this.state.realStateListings} />
                 )}
             </div>
         )
@@ -45,6 +201,11 @@ class Home extends React.Component {
 
 class ClassicCarsHome extends React.Component {
     render() {
+        const { listings } = this.props
+
+        // Rendering thumbnails based on listings
+        const ls = listings.map((l, ix) => <CarThumbnail listing={l} />)
+
         return (
             <div className="home-classic-cars-container">
                 <div className="showcase-container">
@@ -66,8 +227,7 @@ class ClassicCarsHome extends React.Component {
                         <h2>Browse classic cars</h2>
                         <span className="new">new</span>
                     </div>
-                    <div className="listings">
-                    </div>
+                    <div className="listings">{ls}</div>
                 </div>
             </div>
         )
@@ -76,6 +236,11 @@ class ClassicCarsHome extends React.Component {
 
 class RealStateHome extends React.Component {
     render() {
+        const { listings } = this.props
+
+        // Rendering thumbnails based on listings
+        const ls = listings.map((l, ix) => <RealStateThumbnail listing={l} />)
+
         return (
             <div className="home-real-state-container">
                 <div className="showcase-container">
@@ -97,8 +262,7 @@ class RealStateHome extends React.Component {
                         <h2>Browse vacation homes and apartments</h2>
                         <span className="new">new</span>
                     </div>
-                    <div className="listings">
-                    </div>
+                    <div className="listings">{ls}</div>
                 </div>
             </div>
         )
@@ -107,8 +271,28 @@ class RealStateHome extends React.Component {
 
 class CarThumbnail extends React.Component {
     render() {
+        const { listing } = this.props
+
+        // <p className="">{listing.fullPrice}</p>
         return (
-            <div className="car-thumbnail">
+            <div className="thumbnail">
+                <div className="img-wrapper">
+                    <img src={listing.img} alt="Listing" />
+                </div>
+                <div className="content">
+                    <div className="row-1">
+                        <p className="name">{listing.name}</p>
+                        <p className="location">
+                            <img src={Pin} alt={listing.location} />
+                            {listing.location}
+                        </p>
+                    </div>
+                    <div className="row-2">
+                        <p className="share-price">~${listing.sharePrice}</p>
+                        <p className="share-type">{listing.shareType}</p>
+                    </div>
+                    <p className="xrp">{listing.xrp} XRP</p>
+                </div>
             </div>
         )
     }
@@ -116,8 +300,26 @@ class CarThumbnail extends React.Component {
 
 class RealStateThumbnail extends React.Component {
     render() {
+        const { listing } = this.props
+
+        // <p className="">{listing.fullPrice}</p>
         return (
-            <div className="real-state-thumbnail">
+            <div className="thumbnail">
+                <div className="img-wrapper">
+                    <img src={listing.img} alt="Listing" />
+                </div>
+                <div className="content">
+                    <div className="row-1">
+                        <p className="name">{listing.name}</p>
+                        <p className="location">{listing.location}</p>
+                    </div>
+                    <div className="row-2">
+                        <p className="share-price">~${listing.sharePrice}</p>
+                        <p className="share-type">{listing.shareType}</p>
+                    </div>
+                    <p className="specs">{listing.specs}</p>
+                    <p className="xrp">{listing.xrp} XRP</p>
+                </div>
             </div>
         )
     }
